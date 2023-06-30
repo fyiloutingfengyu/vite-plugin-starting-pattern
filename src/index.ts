ad81fbcbcdc190d0a5d88690d6cf2102fd7202fb
felix <1393491999@qq.com>
2023-06-30 17:09:19 +0800

    feat: 新增自定义图案类型和颜色的功能

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import type { Plugin } from 'vite';
 import colors from 'picocolors';
 
-export default function Monitor(): Plugin {
-  console.log('global', global);
-
+export default function startingPattern(type = '', color: string = 'green'): Plugin {
   return {
-    name: 'vite-plugin-monitor',
+    name: 'vite-plugin-starting-pattern',
     apply: 'serve',
     config(userConfig, env) {
-      console.log('666 userConfig', userConfig);
-      console.log('777 env', env);
       // 可以做进一步的修改，会自动合入当前的配置
     },
     configureServer(server) {
       const print = server.printUrls;
+      const outPutLogInfo = (str) => {
+        console.log(colors[color](str));
+      };
 
       server.printUrls = () => {
         const network = server.resolvedUrls?.network[0];
@@ -21,14 +20,27 @@ export default function Monitor(): Plugin {
 
         if (!network && !local) {
           console.log(colors.red('获取IP地址失败，请检查vite.config.ts文件中server.host配置是否正确！'));
-        } else {
-          console.log(colors.green(
-            '|_____ *** success *** _____|\n ' +
-            '|----******************----| \n' +
-            '|---********************---| \n'
-          ));
-          print();
+          return;
         }
+
+        // todo f 完善type
+        // todo f 配置颜色
+        switch (type) {
+          case 'type1':
+            outPutLogInfo('*****type1*****');
+            break;
+          case 'type2':
+            outPutLogInfo('*****type2*****');
+            break;
+          default:
+            outPutLogInfo('|____ *** success *** ____|\n ' +
+              '|----******************----| \n' +
+              '|---********************---| \n');
+            break;
+        }
+        print();
+
+
       };
     }
   };
